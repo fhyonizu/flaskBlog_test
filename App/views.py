46d5495eedbc7e4df932193328b4750d7bab44c2
Hu Yi <fhyoni@163.com>
2024-08-21 12:37:16 +0800

    1.1
    完成留言功能

--- App/views.py
+++ App/views.py
@@ -4,7 +4,7 @@ from .models import *
 blue = Blueprint('article', __name__)
 
 @blue.route('/')
-def base():
+def index():
     articles = get_artics()
     return render_template('index2.html', articles=articles)
 
@@ -16,7 +16,7 @@ def get_artics():
 @blue.route('/words/<int:article_id>')
 def get_words(article_id):
     article_id = Article.query.get(article_id)
-    return render_template('words.html',article_id = article_id)
+    return render_template('words.html',article = article_id)
 
 @blue.route('/add_comment/<int:article_id>', methods=['POST'])
 def add_comment(article_id):
@@ -25,4 +25,16 @@ def add_comment(article_id):
         new_comment = Comment(article_id=article_id, text=comment_text)
         db.session.add(new_comment)
         db.session.commit()
-    return redirect(url_for('user.base'))
+    return redirect(url_for('article.index'))
+
+@blue.route('/comments/')
+def get_comments():
+    comments = Comment.query.all()
+    comments_with_articles = []
+    for comment in comments:
+        article = Article.query.get(comment.article_id)
+        comments_with_articles.append({
+            'comment': comment,
+            'article': article
+        })
+    return render_template('comments.html', comments_with_articles=comments_with_articles)
